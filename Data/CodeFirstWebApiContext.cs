c4bf18ba793cf9a4ad991c3197b0e2bbef5538de
prathap <prathap@gmail.com>
2024-07-05 11:20:38 +0530

    Add project files.

---
+++ Data/CodeFirstWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeFirstWebApi.Models;
+
+namespace CodeFirstWebApi.Data
+{
+    public class CodeFirstWebApiContext : DbContext
+    {
+        public CodeFirstWebApiContext (DbContextOptions<CodeFirstWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeFirstWebApi.Models.Product> Product { get; set; } = default!;
+    }
+}
